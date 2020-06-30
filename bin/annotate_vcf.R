ba661cf58b825105f23791554c54bbd646e5b163
allydunham <ally@ebi.ac.uk>
2020-06-30 16:23:30 +0100

    Removed unessesary processing

--- bin/annotate_vcf.R
+++ bin/annotate_vcf.R
@@ -15,25 +15,4 @@ txdb <- TxDb.Scerevisiae.UCSC.sacCer3.sgdGene
 #seqlevels(txdb) <- paste0('chromosome',c(1:16,'M'))
 
 coding <- data.frame(predictCoding(vcf, txdb, seqSource=sc))
-
-## Function to get the correct alt allele based on a coding mutant row
-getALT <- function(x){
-  if (x$strand == '-'){
-    return(as.character(reverseComplement(DNAString(x$varAllele))))
-  } else {
-    return(x$varAllele)
-  }
-}
-
-coding$ALT <- apply(coding, 1, getALT)
-
-## Fix coding for mutfunc
-coding <- coding[coding$width == 1 & !is.na(coding$ALT),]
-coding$mut_id <- paste0(coding$seqnames,':',coding$start,'_',coding$REF,'/',coding$ALT)
-coding <- coding[c('mut_id', 'REFCODON', 'VARCODON', 'PROTEINLOC',
-                   'REFAA', 'VARAA', 'GENEID', 'CONSEQUENCE')]
-
-colnames(coding) <- c('mut_id','ref_codon','alt_codon','pos_aa',
-                      'ref_aa','alt_aa','gene','type')
-
 write.table(coding, args[2], sep='\t', row.names=F, quote=F)

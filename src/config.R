36b4c8a3ed536adda3e7b6f31f7b251f7eb2745b
allydunham <ally@ebi.ac.uk>
2020-07-01 16:56:46 +0100

    Added uniprot map

--- src/config.R
+++ src/config.R
@@ -2,3 +2,10 @@
 # Setup namespace for Yeast Strains project
 
 library(tidyverse)
+
+# Import Uniprot to Gene map
+NAME_TO_UNIPROT <- read_tsv('meta/uniprot_map', col_names = c('uniprot', 'database', 'id')) %>%
+    filter(database == 'EnsemblGenome') %>%
+    select(uniprot, id) %>%
+    drop_na()
+NAME_TO_UNIPROT <- structure(NAME_TO_UNIPROT$uniprot, names = NAME_TO_UNIPROT$id)

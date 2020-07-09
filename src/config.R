811378f785859092cde47c0fafed6e20a22b8638
allydunham <ally@ebi.ac.uk>
2020-07-09 17:29:37 +0100

    Added some standard packages and set default plot theme

--- src/config.R
+++ src/config.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 # Setup namespace for Yeast Strains project
 library(tidyverse)
+library(ggpubr)
+library(ggtext)
 library(broom)
 library(plotlistr)
 
@@ -10,3 +12,10 @@ NAME_TO_UNIPROT <- read_tsv('meta/uniprot_map', col_names = c('uniprot', 'databa
     select(uniprot, id) %>%
     drop_na()
 NAME_TO_UNIPROT <- structure(NAME_TO_UNIPROT$uniprot, names = NAME_TO_UNIPROT$id)
+
+# GGPlot Theme
+theme_set(theme_pubclean() + theme(legend.position = 'right',
+                                   plot.title = element_text(hjust = 0.5),
+                                   plot.subtitle = element_text(hjust = 0.5),
+                                   strip.background = element_blank(),
+                                   legend.key = element_blank()))
